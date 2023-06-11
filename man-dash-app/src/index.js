4ab908278fe61f3e845183dba16fae6005edfc94
MattiaOlia <mattiaolia97@gmail.com>
2023-06-11 20:29:32 +0100

    deployed

--- man-dash-app/src/index.js
+++ man-dash-app/src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
     <Router>
       <Routes>
         
-        <Route path="/" element={<App />} />
+        <Route path="/TaskTracker" element={<App />} />
         <Route path="/tasks" element={<TaskPage />} />
         <Route path="/members" element={<MemberPage />}/>
         <Route path="/calander" element={<CalanderPage />} />
